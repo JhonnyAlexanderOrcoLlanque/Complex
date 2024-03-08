fb8fd6cbd22e5c8cb68efab7874979f2e079a87f
JhonnyAlexanderOrcoLlanque <jhonnyorco4@gmail.com>
2024-03-07 22:40:27 -0400

    v1.2
    se terminaron de implementar las funciones module() y squareRoot()

--- Complex.java
+++ Complex.java
@@ -4,11 +4,9 @@ OPERACIÓN DE NUMEROS COMPLEJOS
 @autor Jhonny Alexander Orco Llanque
 @autor Felix Maturano Zarate
 @autor German Basualdo Rengifo
-@version 1.1
+@version 1.2
 @date 07/03/2024
 */
-//Hola
-// Continuamos
 
 class Complex
 {
@@ -21,37 +19,37 @@ class Complex
         this.imaginario = _imaginario;
     }
     
-    Complex sumar(Complex otro) {
+    Complex addition(Complex otro) {
         return new Complex(real + otro.real, imaginario + otro.imaginario);
     }
   
-    Complex restar(Complex otro) {
+    Complex substraction(Complex otro) {
         return new Complex(real - otro.real, imaginario - otro.imaginario);
     }
   
-    Complex multiplicar(Complex otro) {
+    Complex multiplication(Complex otro) {
         double r = real * otro.real - imaginario * otro.imaginario;
         double i = real * otro.imaginario + imaginario * otro.real;
         return new Complex(r, i);
     }
   
-    Complex dividir(Complex otro) {
+    Complex division(Complex otro) {
         double divisor = otro.real * otro.real + otro.imaginario * otro.imaginario;
         double r = (real * otro.real + imaginario * otro.imaginario) / divisor;
         double i = (imaginario * otro.real - real * otro.imaginario) / divisor;
         return new Complex(r, i);
     }
     
-    double modulo(){
+    double module(){
         //Jhonny Alexander Orco Llanque
         double result = Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imaginario, 2));
         return result;
     }
 
-    Complex raizCuadrada(){
+    Complex squareRoot(){
         //Jhonny Alexander Orco Llanque
-        double r = Math.sqrt( (this.modulo()+this.real)/2 );
-        double i = ( Math.signum(this.imaginario) * Math.sqrt(this.modulo()-this.real)) / Math.sqrt(2);
+        double r = Math.sqrt( (this.module()+this.real)/2 );
+        double i = ( Math.signum(this.imaginario) * Math.sqrt(this.module()-this.real)) / Math.sqrt(2);
         return new Complex(r, i);
     }
 
@@ -70,13 +68,13 @@ class Complex
         
         System.out.println("Complejo1: " + c1.toString());
         System.out.println("Complejo2: " + c2.toString());
-        System.out.println("Suma: " + c1.sumar(c2));
-        System.out.println("Resta: " + c1.restar(c2));
-        System.out.println("Multiplicacion: " + c1.multiplicar(c2));
-        System.out.println("Division: " + c1.dividir(c2));
-        System.out.println("Modulo de complejo 1: " + c1.modulo());
-        System.out.println("Modulo de complejo 2: " + c2.modulo());
-        System.out.println("Raiz cuadrada de complejo 1: +/- (" +  c1.raizCuadrada() + ")");
-        System.out.println("Raiz cuadrada de complejo 2: +/- (" + c2.raizCuadrada() + ")");
+        System.out.println("Suma: " + c1.addition(c2));
+        System.out.println("Resta: " + c1.substraction(c2));
+        System.out.println("Multiplicacion: " + c1.multiplication(c2));
+        System.out.println("Division: " + c1.division(c2));
+        System.out.println("Modulo de complejo 1: " + c1.module());
+        System.out.println("Modulo de complejo 2: " + c2.module());
+        System.out.println("Raiz cuadrada de complejo 1: +/- (" +  c1.squareRoot() + ")");
+        System.out.println("Raiz cuadrada de complejo 2: +/- (" + c2.squareRoot() + ")");
     }
 }
